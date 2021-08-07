adf57b114886d0cd722684cc5980a6d27bb0707f
Matt Hillsdon <matt.hillsdon@microbit.org>
2021-08-07 20:42:15 +0100

    Avoid negative look behind for Safari.
    It's a syntax error so immediately scuppers things for Safari.
    
    This seems to be sufficient to get it running. Minimal testing so far
    but a big step forward.
    
    This specific change seems safe and well tested.

--- packages/pyright-internal/src/analyzer/docStringConversion.ts
+++ packages/pyright-internal/src/analyzer/docStringConversion.ts
@@ -77,7 +77,7 @@ const WhitespaceRegExp = /\s/g;
 const DoubleTickRegExp = /``/g;
 const TildeRegExp = /~/g;
 const PlusRegExp = /\+/g;
-const UnescapedMarkdownCharsRegExp = /(?<!\\)([_*~[\]])/g;
+const MarkdownCharsRegExp = /[_*~[\]]/g;
 const linkRegExp = /(\[.*\]\(.*\))/g;
 
 const HtmlEscapes: RegExpReplacement[] = [
@@ -377,7 +377,15 @@ class DocStringConverter {
                 if (linkRegExp.test(item)) {
                     this._append(item);
                 } else {
-                    this._append(item.replace(UnescapedMarkdownCharsRegExp, '\\$1'));
+                    // Negative lookbehind not supported in Safari so handle escapes separately.
+                    this._append(
+                        item.replace(MarkdownCharsRegExp, (match: string, offset: number, str: string) => {
+                            if (str.charAt(offset - 1) === '\\') {
+                                return match;
+                            }
+                            return '\\' + match;
+                        })
+                    );
                 }
             });
         }
