b73e995b03ffa696932164b5702609800e995d2d
DetachHead <detachhead@users.noreply.github.com>
2024-02-25 11:08:17 +1000

    add test for context managers that return `bool | None`, `False | None` and `True | None`

---
+++ packages/pyright-internal/src/tests/samples/withBased.py
@@ -0,0 +1,48 @@
+import contextlib
+from types import TracebackType
+from typing import Iterator, Literal
+
+from typing_extensions import assert_never
+
+class BoolOrNone(contextlib.AbstractContextManager[None]):
+    def __exit__(
+        self,
+        __exc_type: type[BaseException] | None,
+        __exc_value: BaseException | None,
+        __traceback: TracebackType | None,
+    ) -> bool | None:
+        ...
+
+def _():
+    with BoolOrNone():
+        raise Exception
+    print(1)  # reachable
+
+class TrueOrNone(contextlib.AbstractContextManager[None]):
+    def __exit__(
+        self,
+        __exc_type: type[BaseException] | None,
+        __exc_value: BaseException | None,
+        __traceback: TracebackType | None,
+    ) -> Literal[True] | None:
+        ...
+
+def _():
+    with TrueOrNone():
+        raise Exception
+    print(1)  # reachable
+
+
+class FalseOrNone(contextlib.AbstractContextManager[None]):
+    def __exit__(
+        self,
+        __exc_type: type[BaseException] | None,
+        __exc_value: BaseException | None,
+        __traceback: TracebackType | None,
+    ) -> Literal[False] | None:
+        ...
+
+def _():
+    with FalseOrNone():
+        raise Exception
+    print(1)  # unreachable
