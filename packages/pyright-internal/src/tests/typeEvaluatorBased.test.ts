91ef6e6cb28af2acecd28a5dc96742ed25548143
DetachHead <detachhead@users.noreply.github.com>
2024-03-24 17:04:59 +1000

    add test for `reportUnexportedImportUsage`

--- packages/pyright-internal/src/tests/typeEvaluatorBased.test.ts
+++ packages/pyright-internal/src/tests/typeEvaluatorBased.test.ts
@@ -1,7 +1,7 @@
 import { BasedConfigOptions, ConfigOptions } from '../common/configOptions';
 import { DiagnosticRule } from '../common/diagnosticRules';
 import { Uri } from '../common/uri/uri';
-import { typeAnalyzeSampleFiles, validateResultsButBased } from './testUtils';
+import { resolveSampleFilePath, typeAnalyzeSampleFiles, validateResultsButBased } from './testUtils';
 
 test('reportUnreachable', () => {
     const configOptions = new ConfigOptions(Uri.empty());
@@ -38,5 +38,28 @@ test('default typeCheckingMode=all', () => {
         ],
         infos: [{ line: 95 }, { line: 98 }],
         unusedCodes: [{ line: 102 }],
+    });
+});
+
+test('reportUnexportedImportUsage', () => {
+    const configOptions = new ConfigOptions(Uri.empty());
+    configOptions.diagnosticRuleSet.reportUnexportedImportUsage = 'error';
+    //TODO: typeAnalyzeSampleFiles should probably do this by default
+    configOptions.projectRoot = Uri.file(resolveSampleFilePath('based_implicit_re_export'));
+    const analysisResults = typeAnalyzeSampleFiles(['based_implicit_re_export/baz.py'], configOptions);
+    console.log(analysisResults);
+    validateResultsButBased(analysisResults, {
+        errors: [
+            {
+                line: 0,
+                code: DiagnosticRule.reportUnexportedImportUsage,
+                message: '"a" is not exported from module "asdf.bar"\n  Import from "asdf.foo" instead',
+            },
+            {
+                line: 0,
+                code: DiagnosticRule.reportUnexportedImportUsage,
+                message: '"b" is not exported from module "asdf.bar"\n  Import from "asdf.foo" instead',
+            },
+        ],
     });
 });
