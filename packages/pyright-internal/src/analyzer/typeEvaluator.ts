microsoft/pyright

add rule to detect private non-py.typed import usages

packages/pyright-internal/src/analyzer/typeEvaluator.ts
@@ -5499,6 +5499,15 @@ export function createTypeEvaluator(importLookup: ImportLookup, evaluatorOptions
                             }),
                             node.memberName
                         );
+                    } else if (symbol.isPrivateNonPyTypedImport()) {
+                        addDiagnostic(
+                            DiagnosticRule.reportPrivateNonPyTypedImportUsage,
+                            LocMessage.privateImportFromPyTypedModule().format({
+                                name: memberName,
+                                module: baseType.moduleName,
+                            }),
+                            node.memberName
+                        );
                     }
                 } else {
                     // Does the module export a top-level __getattr__ function?
@@ -19050,21 +19059,24 @@ export function createTypeEvaluator(importLookup: ImportLookup, evaluatorOptions
                     node.name
                 );
             }
-
-            if (resolvedAliasInfo.privatePyTypedImporter) {
+            const privateImporter =
+                resolvedAliasInfo.privatePyTypedImporter || resolvedAliasInfo.privateNonPyTypedImporter;
+            if (privateImporter) {
                 const diag = new DiagnosticAddendum();
-                if (resolvedAliasInfo.privatePyTypedImported) {
+                if (resolvedAliasInfo.privateImported) {
                     diag.addMessage(
                         LocAddendum.privateImportFromPyTypedSource().format({
-                            module: resolvedAliasInfo.privatePyTypedImported,
+                            module: resolvedAliasInfo.privateImported,
                         })
                     );
                 }
                 addDiagnostic(
-                    DiagnosticRule.reportPrivateImportUsage,
+                    resolvedAliasInfo.privatePyTypedImporter
+                        ? DiagnosticRule.reportPrivateImportUsage
+                        : DiagnosticRule.reportPrivateNonPyTypedImportUsage,
                     LocMessage.privateImportFromPyTypedModule().format({
                         name: node.name.value,
-                        module: resolvedAliasInfo.privatePyTypedImporter,
+                        module: privateImporter,
                     }) + diag.getString(),
                     node.name
                 );

packages/pyright-internal/src/common/configOptions.ts
@@ -390,6 +390,9 @@ export interface DiagnosticRuleSet {
 
     // Report ignore comments without a specified rule
     reportIgnoreCommentWithoutRule: DiagnosticLevel;
+
+    // the "works properly" version of reportPrivateImportUsage
+    reportPrivateNonPyTypedImportUsage: DiagnosticLevel;
 }
 
 export function cloneDiagnosticRuleSet(diagSettings: DiagnosticRuleSet): DiagnosticRuleSet {
@@ -618,6 +621,7 @@ export function getOffDiagnosticRuleSet(): DiagnosticRuleSet {
         reportUnreachable: 'none',
         reportAny: 'none',
         reportIgnoreCommentWithoutRule: 'none',
+        reportPrivateNonPyTypedImportUsage: 'none',
     };
 
     return diagSettings;
@@ -723,6 +727,7 @@ export function getBasicDiagnosticRuleSet(): DiagnosticRuleSet {
         reportUnreachable: 'none',
         reportAny: 'none',
         reportIgnoreCommentWithoutRule: 'none',
+        reportPrivateNonPyTypedImportUsage: 'none',
     };
 
     return diagSettings;
@@ -828,6 +833,7 @@ export function getStandardDiagnosticRuleSet(): DiagnosticRuleSet {
         reportUnreachable: 'none',
         reportAny: 'none',
         reportIgnoreCommentWithoutRule: 'none',
+        reportPrivateNonPyTypedImportUsage: 'none',
     };
 
     return diagSettings;
@@ -932,6 +938,7 @@ export const getAllDiagnosticRuleSet = (): DiagnosticRuleSet => ({
     reportUnreachable: 'error',
     reportAny: 'error',
     reportIgnoreCommentWithoutRule: 'error',
+    reportPrivateNonPyTypedImportUsage: 'error',
 });
 
 export function getStrictDiagnosticRuleSet(): DiagnosticRuleSet {
@@ -1034,6 +1041,7 @@ export function getStrictDiagnosticRuleSet(): DiagnosticRuleSet {
         reportUnreachable: 'none',
         reportAny: 'none',
         reportIgnoreCommentWithoutRule: 'none',
+        reportPrivateNonPyTypedImportUsage: 'none',
     };
 
     return diagSettings;

DetachHead/basedpyright

packages/pyright-internal/src/analyzer/typeEvaluator.ts
@@ -5499,6 +5499,15 @@ export function createTypeEvaluator(importLookup: ImportLookup, evaluatorOptions
                             }),
                             node.memberName
                         );
+                    } else if (symbol.isPrivateNonPyTypedImport()) {
+                        addDiagnostic(
+                            DiagnosticRule.reportUnexportedImportUsage,
+                            LocMessage.privateImportFromPyTypedModule().format({
+                                name: memberName,
+                                module: baseType.moduleName,
+                            }),
+                            node.memberName
+                        );
                     }
                 } else {
                     // Does the module export a top-level __getattr__ function?
@@ -19050,21 +19059,24 @@ export function createTypeEvaluator(importLookup: ImportLookup, evaluatorOptions
                     node.name
                 );
             }
-
-            if (resolvedAliasInfo.privatePyTypedImporter) {
+            const privateImporter =
+                resolvedAliasInfo.privatePyTypedImporter || resolvedAliasInfo.privateNonPyTypedImporter;
+            if (privateImporter) {
                 const diag = new DiagnosticAddendum();
-                if (resolvedAliasInfo.privatePyTypedImported) {
+                if (resolvedAliasInfo.privateImported) {
                     diag.addMessage(
                         LocAddendum.privateImportFromPyTypedSource().format({
-                            module: resolvedAliasInfo.privatePyTypedImported,
+                            module: resolvedAliasInfo.privateImported,
                         })
                     );
                 }
                 addDiagnostic(
-                    DiagnosticRule.reportPrivateImportUsage,
+                    resolvedAliasInfo.privatePyTypedImporter
+                        ? DiagnosticRule.reportPrivateImportUsage
+                        : DiagnosticRule.reportUnexportedImportUsage,
                     LocMessage.privateImportFromPyTypedModule().format({
                         name: node.name.value,
-                        module: resolvedAliasInfo.privatePyTypedImporter,
+                        module: privateImporter,
                     }) + diag.getString(),
                     node.name
                 );

packages/pyright-internal/src/common/configOptions.ts
@@ -390,6 +390,9 @@ export interface DiagnosticRuleSet {
 
     // Report ignore comments without a specified rule
     reportIgnoreCommentWithoutRule: DiagnosticLevel;
+
+    // the "works properly" version of reportPrivateImportUsage
+    reportUnexportedImportUsage: DiagnosticLevel;
 }
 
 export function cloneDiagnosticRuleSet(diagSettings: DiagnosticRuleSet): DiagnosticRuleSet {
@@ -618,6 +621,7 @@ export function getOffDiagnosticRuleSet(): DiagnosticRuleSet {
         reportUnreachable: 'none',
         reportAny: 'none',
         reportIgnoreCommentWithoutRule: 'none',
+        reportUnexportedImportUsage: 'none',
     };
 
     return diagSettings;
@@ -723,6 +727,7 @@ export function getBasicDiagnosticRuleSet(): DiagnosticRuleSet {
         reportUnreachable: 'none',
         reportAny: 'none',
         reportIgnoreCommentWithoutRule: 'none',
+        reportUnexportedImportUsage: 'none',
     };
 
     return diagSettings;
@@ -828,6 +833,7 @@ export function getStandardDiagnosticRuleSet(): DiagnosticRuleSet {
         reportUnreachable: 'none',
         reportAny: 'none',
         reportIgnoreCommentWithoutRule: 'none',
+        reportUnexportedImportUsage: 'none',
     };
 
     return diagSettings;
@@ -932,6 +938,7 @@ export const getAllDiagnosticRuleSet = (): DiagnosticRuleSet => ({
     reportUnreachable: 'error',
     reportAny: 'error',
     reportIgnoreCommentWithoutRule: 'error',
+    reportUnexportedImportUsage: 'error',
 });
 
 export function getStrictDiagnosticRuleSet(): DiagnosticRuleSet {
@@ -1034,6 +1041,7 @@ export function getStrictDiagnosticRuleSet(): DiagnosticRuleSet {
         reportUnreachable: 'none',
         reportAny: 'none',
         reportIgnoreCommentWithoutRule: 'none',
+        reportUnexportedImportUsage: 'none',
     };
 
     return diagSettings;