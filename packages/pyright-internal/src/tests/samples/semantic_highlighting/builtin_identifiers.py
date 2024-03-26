bba994e6e3977036b8a569ecc46d0208722d932e
Omar Valdez <omarantoniovaldezf2@gmail.com>
2024-03-25 21:06:03 -0700

    Test `defaultLibrary` and `builtin` token modifiers

---
+++ packages/pyright-internal/src/tests/samples/semantic_highlighting/builtin_identifiers.py
@@ -0,0 +1,15 @@
+from typing import List, Set, TypeAlias
+
+Foo = list
+Bar = List | Set
+Old: TypeAlias = dict
+type New = tuple
+
+print
+input
+func = print
+
+int
+str
+ValueError
+EnvironmentError
