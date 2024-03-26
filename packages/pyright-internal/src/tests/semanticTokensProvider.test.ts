992e1a87ef2bbac92745318f8c0d8d03441c7a4d
Omar Valdez <omarantoniovaldezf2@gmail.com>
2024-03-25 21:06:03 -0700

    Test `defaultLibrary` and `builtin` token modifiers

--- packages/pyright-internal/src/tests/semanticTokensProvider.test.ts
+++ packages/pyright-internal/src/tests/semanticTokensProvider.test.ts
@@ -11,9 +11,9 @@ if (process.platform !== 'win32' || !process.env['CI']) {
         const result = semanticTokenizeSampleFile('type_annotation.py');
         expect(result).toStrictEqual([
             { type: 'variable', modifiers: [], start: 0, length: 1 },
-            { type: 'class', modifiers: [], start: 3, length: 3 },
+            { type: 'class', modifiers: ['defaultLibrary', 'builtin'], start: 3, length: 3 },
             { type: 'variable', modifiers: [], start: 7, length: 1 },
-            { type: 'class', modifiers: [], start: 10, length: 3 },
+            { type: 'class', modifiers: ['defaultLibrary', 'builtin'], start: 10, length: 3 },
         ]);
     });
 
@@ -73,16 +73,16 @@ if (process.platform !== 'win32' || !process.env['CI']) {
             { type: 'function', modifiers: ['definition'], start: 34, length: 3 },
             { type: 'function', modifiers: [], start: 34, length: 3 },
             { type: 'variable', modifiers: [], start: 38, length: 1 },
-            { type: 'class', modifiers: [], start: 41, length: 3 },
+            { type: 'class', modifiers: ['defaultLibrary', 'builtin'], start: 41, length: 3 },
             { type: 'variable', modifiers: [], start: 47, length: 1 },
             { type: 'variable', modifiers: [], start: 52, length: 1 },
-            { type: 'class', modifiers: [], start: 58, length: 3 },
+            { type: 'class', modifiers: ['defaultLibrary', 'builtin'], start: 58, length: 3 },
             { type: 'function', modifiers: [], start: 72, length: 3 },
             { type: 'type', modifiers: [], start: 79, length: 3 },
             { type: 'class', modifiers: [], start: 85, length: 8 },
             { type: 'function', modifiers: [], start: 105, length: 3 },
             { type: 'class', modifiers: [], start: 110, length: 8 },
-            { type: 'class', modifiers: [], start: 120, length: 3 },
+            { type: 'class', modifiers: ['defaultLibrary', 'builtin'], start: 120, length: 3 },
         ]);
     });
     test('undefined', () => {
@@ -95,14 +95,51 @@ if (process.platform !== 'win32' || !process.env['CI']) {
             { type: 'namespace', modifiers: [], start: 5, length: 6 },
             { type: 'class', modifiers: [], start: 19, length: 9 },
             { type: 'class', modifiers: [], start: 19, length: 9 },
-            { type: 'class', modifiers: [], start: 30, length: 3 },
-            { type: 'class', modifiers: [], start: 36, length: 3 },
+            { type: 'class', modifiers: ['defaultLibrary', 'builtin'], start: 30, length: 3 },
+            { type: 'class', modifiers: ['defaultLibrary', 'builtin'], start: 36, length: 3 },
             { type: 'class', modifiers: [], start: 40, length: 3 },
             { type: 'class', modifiers: [], start: 45, length: 9 },
-            { type: 'class', modifiers: [], start: 57, length: 3 },
+            { type: 'class', modifiers: ['defaultLibrary', 'builtin'], start: 57, length: 3 },
             { type: 'keyword', modifiers: [], start: 61, length: 4 },
             { type: 'class', modifiers: [], start: 66, length: 3 },
-            { type: 'class', modifiers: [], start: 72, length: 3 },
+            { type: 'class', modifiers: ['defaultLibrary', 'builtin'], start: 72, length: 3 },
+        ]);
+    });
+
+    test('builtins', () => {
+        const result = semanticTokenizeSampleFile('builtin_identifiers.py');
+        expect(result).toStrictEqual([
+            // imports
+            // TODO: Fix duplicates
+            { type: 'namespace', modifiers: [], start: 5, length: 6 }, // typing
+            { type: 'class', modifiers: [], start: 19, length: 4 }, // List
+            { type: 'class', modifiers: [], start: 19, length: 4 },
+            { type: 'class', modifiers: [], start: 25, length: 3 }, // Set
+            { type: 'class', modifiers: [], start: 25, length: 3 },
+            { type: 'class', modifiers: [], start: 30, length: 9 }, // TypeAlias
+            { type: 'class', modifiers: [], start: 30, length: 9 },
+            // type aliases
+            { type: 'class', modifiers: ['defaultLibrary', 'builtin'], start: 41, length: 3 }, // Foo
+            { type: 'class', modifiers: ['defaultLibrary', 'builtin'], start: 47, length: 4 }, // list
+            { type: 'type', modifiers: [], start: 52, length: 3 }, // Bar
+            { type: 'class', modifiers: [], start: 58, length: 4 }, // List
+            { type: 'class', modifiers: [], start: 65, length: 3 }, // Set
+            { type: 'class', modifiers: [], start: 69, length: 3 }, // Old
+            { type: 'class', modifiers: [], start: 74, length: 9 }, // TypeAlias
+            { type: 'class', modifiers: ['defaultLibrary', 'builtin'], start: 86, length: 4 }, // dict
+            { type: 'keyword', modifiers: [], start: 91, length: 4 }, // type
+            { type: 'class', modifiers: [], start: 96, length: 3 }, // New
+            { type: 'class', modifiers: ['defaultLibrary', 'builtin'], start: 102, length: 5 }, // tuple
+            // builtin functions
+            { type: 'function', modifiers: ['defaultLibrary', 'builtin'], start: 109, length: 5 }, // print
+            { type: 'function', modifiers: ['defaultLibrary', 'builtin'], start: 115, length: 5 }, // input
+            { type: 'function', modifiers: ['defaultLibrary', 'builtin'], start: 121, length: 4 }, // func
+            { type: 'function', modifiers: ['defaultLibrary', 'builtin'], start: 128, length: 5 }, // print
+            // builtin types/classes
+            { type: 'class', modifiers: ['defaultLibrary', 'builtin'], start: 135, length: 3 }, // int
+            { type: 'class', modifiers: ['defaultLibrary', 'builtin'], start: 139, length: 3 }, // str
+            { type: 'class', modifiers: ['defaultLibrary', 'builtin'], start: 143, length: 10 }, // ValueError
+            { type: 'class', modifiers: ['defaultLibrary', 'builtin'], start: 154, length: 16 }, // EnvironmentError
         ]);
     });
 } else {
