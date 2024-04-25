77bcc3de7f2db6e82f48231c45356945dced72fa
detachhead <detachhead@users.noreply.github.com>
2024-04-26 01:55:58 +1000

    ignore type errors from untyped nodejs_wheel package

--- basedpyright/run_node.py
+++ basedpyright/run_node.py
@@ -3,8 +3,8 @@ from __future__ import annotations
 import sys
 from pathlib import Path
 
-from nodejs_wheel import executable
+from nodejs_wheel import executable  # pyright:ignore[reportMissingTypeStubs]
 
 
 def run(script_name: str):
-    sys.exit(executable.call_node(Path(__file__).parent / f"{script_name}.js", *sys.argv[1:]))
+    sys.exit(executable.call_node(Path(__file__).parent / f"{script_name}.js", *sys.argv[1:]))  # pyright:ignore[reportUnknownMemberType]
