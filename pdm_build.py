7f8755f77befe452f3700255b53c90e3a67eb23b
detachhead <detachhead@users.noreply.github.com>
2024-05-08 13:53:13 +1000

    remove hacky workarounds for calling node now that there's an api for it

--- pdm_build.py
+++ pdm_build.py
@@ -1,30 +1,27 @@
 from __future__ import annotations
 
-import sys
 from json import loads
 from pathlib import Path
 from shutil import copyfile, copytree
-from subprocess import run  # noqa: S404
 from typing import TypedDict, cast
 
-from nodejs_wheel.executable import ROOT_DIR  # pyright:ignore[reportMissingTypeStubs]
-
-node_exe = Path(ROOT_DIR, ("node.exe" if sys.platform == "win32" else "bin/node"))
-
-npm_script = Path(ROOT_DIR, "lib", "node_modules", "npm", "bin", "npm-cli.js")
-
-
-# Remove when https://github.com/njzjz/nodejs-wheel/pull/24 is merged
-def npm(cmd: list[str]):
-    _ = run([node_exe, npm_script, *cmd], check=True)  # noqa: S603
+from nodejs_wheel.executable import (  # pyright:ignore[reportMissingTypeStubs]
+    npm,  # pyright:ignore[reportUnknownVariableType]
+)
 
 
 class PackageJson(TypedDict):
     bin: dict[str, str]
 
 
-npm(["ci"])
-npm(["run", "build:cli:dev"])
+def run_npm(*args: str):
+    exit_code = npm(args)
+    if exit_code != 0:
+        raise Exception(f"the following npm command exited with {exit_code=}: {args}")
+
+
+run_npm("ci")
+run_npm("run", "build:cli:dev")
 
 npm_package_dir = Path("packages/pyright")
 pypi_package_dir = Path("basedpyright")
