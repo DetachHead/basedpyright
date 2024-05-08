cf89836a4dc627cdaf8284ea1d13aaea31722b7e
detachhead <detachhead@users.noreply.github.com>
2024-05-08 13:53:13 +1000

    remove hacky workarounds for calling node now that there's an api for it

--- basedpyright/run_node.py
+++ basedpyright/run_node.py
@@ -3,8 +3,10 @@ from __future__ import annotations
 import sys
 from pathlib import Path
 
-from nodejs_wheel import executable  # pyright:ignore[reportMissingTypeStubs]
+from nodejs_wheel.executable import (  # pyright:ignore[reportMissingTypeStubs]
+    node,  # pyright:ignore[reportUnknownVariableType]
+)
 
 
 def run(script_name: str):
-    sys.exit(executable.call_node(Path(__file__).parent / f"{script_name}.js", *sys.argv[1:]))  # pyright:ignore[reportUnknownMemberType]
+    sys.exit(node([Path(__file__).parent / f"{script_name}.js", *sys.argv[1:]]))
