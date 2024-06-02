a92ab39568d95e44d7e1366440a2ff6c71c2480b
detachhead <detachhead@users.noreply.github.com>
2024-06-02 19:16:40 +1000

    fix values that were narrowed to `Never` being coloured as a type

--- packages/pyright-internal/src/tests/samples/semantic_highlighting/never.py
+++ packages/pyright-internal/src/tests/samples/semantic_highlighting/never.py
@@ -5,3 +5,15 @@ bar = Never
 
 def baz() -> Never:
     ...
+
+def asdf(foo: Never):
+    value: Never = foo
+    value
+
+Type = Never
+value: Type = 1
+
+def inferred():
+    value = ''
+    if not isinstance(value, str):
+        value
