4a62f24172b0a5197b856c09a6bf1265e58bab3c
detachhead <detachhead@users.noreply.github.com>
2024-08-13 16:45:35 +1000

    fix broken stub docstrings from python 3.8 taking precedence over docstrings from 3.12

--- based_build/generateAllDocstubs.sh
+++ based_build/generateAllDocstubs.sh
@@ -1,4 +1,4 @@
-for pythonVersion in 3.8 3.9 3.10 3.11 3.12; do
+for pythonVersion in 3.12 3.11 3.10 3.9 3.8; do
     ./pw pdm use $pythonVersion --first
     ./pw pdm install --group=docstubs --no-self --no-default
     ./pw pdm run generate_docstubs
