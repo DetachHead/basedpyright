b245efda2db1fcd30f1203294e33576e22191eae
detachhead <detachhead@users.noreply.github.com>
2024-08-17 19:15:50 +1000

    don't `reportUnusedParameter` on dunders and do report them on old positional arguments

--- packages/pyright-internal/src/tests/samples/reportUnusedParameter.py
+++ packages/pyright-internal/src/tests/samples/reportUnusedParameter.py
@@ -2,19 +2,25 @@ from abc import abstractmethod
 from typing import override
 
 
-def foo(value: int): ...
+def foo(value: int): ... # error
 
 class Foo:
     @abstractmethod
-    def foo(self, asdf: int): ...
+    def foo(self, asdf: int): ...  # no error, abstract method
 
-    def bar(self, asdf: int): ...
+    def bar(self, asdf: int): ...  # error
 
 class Bar(Foo):
     @override
-    def foo(self, asdf: int): ...
+    def foo(self, asdf: int): ...  # no error, override
 
     @override
-    def bar(self, asdf: int): ...
+    def bar(self, asdf: int): ...  # no error, override
+
+    def __baz__(self, asdf: int):  # no error, dunder
+        ...
+
+    def qux(self, __asdf: int):  # error, unused cringe positional argument
+        ...
 
 def bar(_value: int): ...
