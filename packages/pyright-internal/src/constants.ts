b4e6171822ec086e73fc64af4ad5eecaa6ad6f53
detachhead <detachhead@users.noreply.github.com>
2024-09-30 23:39:48 +1000

    fix hyperlinks to the docs on diagnostics in the language server

--- packages/pyright-internal/src/constants.ts
+++ packages/pyright-internal/src/constants.ts
@@ -2,4 +2,4 @@ import version from './version.json';
 
 export const toolName = 'basedpyright';
 
-export const website = `https://basedpyright.com/v${version}`;
+export const website = `https://docs.basedpyright.com/v${version}`;
