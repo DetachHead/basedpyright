de1267e7e423d8ded7d4e08aa170a5fe49645ae9
detachhead <detachhead@users.noreply.github.com>
2024-11-06 18:11:00 +1000

    remove now-redundant check for valid python identifiers in completions now that the same change was made upstream

--- packages/pyright-internal/src/languageService/completionProvider.ts
+++ packages/pyright-internal/src/languageService/completionProvider.ts
@@ -130,7 +130,6 @@ import {
 } from './completionProviderUtils';
 import { DocumentSymbolCollector } from './documentSymbolCollector';
 import { getAutoImportText, getDocumentationPartsForTypeAndDecl } from './tooltipUtils';
-import { Tokenizer } from '../parser/tokenizer';
 
 namespace Keywords {
     const base: string[] = [
@@ -2893,8 +2892,6 @@ export class CompletionProvider {
         paramDetails.params.forEach((paramInfo) => {
             if (
                 paramInfo.param.name &&
-                // filter out any dataclass field aliases that aren't valid identifiers
-                Tokenizer.isPythonIdentifier(paramInfo.param.name) &&
                 paramInfo.kind !== ParamKind.Positional &&
                 paramInfo.kind !== ParamKind.ExpandedArgs
             ) {
