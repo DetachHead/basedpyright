96f7786ee429b9b77df3b4bc2c48bbbd886ec85a
detachhead <detachhead@users.noreply.github.com>
2024-11-13 19:28:01 +1000

    fix "default typeCheckingMode=recommended" test due to typeshed update

--- packages/pyright-internal/src/tests/typeEvaluatorBased.test.ts
+++ packages/pyright-internal/src/tests/typeEvaluatorBased.test.ts
@@ -53,7 +53,6 @@ test('default typeCheckingMode=recommended', () => {
             ...[78, 89, 106, 110, 118, 126].map((line) => ({ code: DiagnosticRule.reportUnreachable, line })),
             { line: 19, code: DiagnosticRule.reportUnknownParameterType },
             { line: 33, code: DiagnosticRule.reportUnknownParameterType },
-            { line: 94, code: DiagnosticRule.reportUnnecessaryComparison },
             { line: 102, code: DiagnosticRule.reportUnusedVariable },
             { line: 113, code: DiagnosticRule.reportUnknownParameterType },
             { line: 114, code: DiagnosticRule.reportUnnecessaryIsInstance },
