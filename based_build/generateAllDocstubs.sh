dff7abe83439dc2143f0d2167e24241843730444
detachhead <detachhead@users.noreply.github.com>
2024-11-19 18:51:48 +1000

    fix all dependencies being incorrectly installed in `generateAllDocstubs.sh`

--- based_build/generateAllDocstubs.sh
+++ based_build/generateAllDocstubs.sh
@@ -1,5 +1,5 @@
 for pythonVersion in 3.13 3.12 3.11 3.10 3.9 3.8; do
     ./pw uv venv --python $pythonVersion
     ./pw uv sync --only-group=docstubs --no-install-project
-    ./pw uv run npm run generate-docstubs
+    ./pw uv run --no-sync based_build/generate_docstubs.py
 done
