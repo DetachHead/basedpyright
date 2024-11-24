6999ed4b8efdec70898088b4363929a32e9ffc61
detachhead <detachhead@users.noreply.github.com>
2024-11-24 10:41:45 +1000

    move all build related scripts into the upstream build folder and baseline basedpyright errors for files with errors that come from upstream

--- based_build/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-this folder only exists because the `build` folder has upstream code full of basedpyright and
-ruff errors, so these scripts serve the same purpose but this one has basedpyright and ruff enabled
-"""
