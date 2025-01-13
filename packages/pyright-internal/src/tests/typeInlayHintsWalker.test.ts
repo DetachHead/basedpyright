a5a472c4552dba05dac1b0c89d05393da418eeda
detachhead <detachhead@users.noreply.github.com>
2025-01-13 11:23:58 +1000

    fix inlay hint tests and use `typed-jest-expect` because jest sucks

--- packages/pyright-internal/src/tests/typeInlayHintsWalker.test.ts
+++ packages/pyright-internal/src/tests/typeInlayHintsWalker.test.ts
@@ -1,59 +1,75 @@
+import { ImportTrackerResults } from '../analyzer/typePrinter';
 import { inlayHintSampleFile } from './testUtils';
+import { tExpect } from 'typed-jest-expect';
+
+const noImports: ImportTrackerResults = { imports: new Set(), importFroms: new Map() };
 
 //TODO: these tests have different start positions in ci on windows, i assume because of crlf moment
 if (process.platform !== 'win32' || !process.env['CI']) {
     test('variables', () => {
         const result = inlayHintSampleFile('variables.py');
-        expect(result).toStrictEqual([
+        tExpect(result).toStrictEqual([
             {
                 inlayHintType: 'variable',
                 position: 102,
                 value: ': str',
+                imports: noImports,
             },
             {
                 inlayHintType: 'variable',
                 position: 408,
                 value: ': TypeAlias',
+                imports: { importFroms: new Map([['typing', new Set(['TypeAlias'])]]), imports: new Set() },
             },
             {
                 inlayHintType: 'variable',
                 position: 569,
                 value: ': Foo',
+                imports: noImports,
             },
             {
                 inlayHintType: 'variable',
                 position: 594,
                 value: ': type[int]',
+                imports: noImports,
             },
             {
                 inlayHintType: 'variable',
                 position: 645,
                 value: ': int | str',
+                imports: noImports,
             },
             {
                 inlayHintType: 'variable',
                 position: 670,
                 value: ': Literal[1, 2]',
+                imports: { importFroms: new Map([['typing', new Set(['Literal'])]]), imports: new Set() },
             },
             {
                 inlayHintType: 'variable',
                 position: 696,
                 value: ': type[int]',
+                imports: noImports,
             },
         ]);
     });
 
     test('function defs', () => {
         const result = inlayHintSampleFile('function_defs.py');
-        expect(result).toStrictEqual([
-            { inlayHintType: 'functionReturn', position: 38, value: '-> None' },
-            { inlayHintType: 'functionReturn', position: 88, value: "-> Literal['']" },
+        tExpect(result).toStrictEqual([
+            { inlayHintType: 'functionReturn', position: 38, value: '-> None', imports: noImports },
+            {
+                inlayHintType: 'functionReturn',
+                position: 88,
+                value: "-> Literal['']",
+                imports: { importFroms: new Map([['typing', new Set(['Literal'])]]), imports: new Set() },
+            },
         ]);
     });
 
     test('function calls', () => {
         const result = inlayHintSampleFile('function_calls.py');
-        expect(result).toStrictEqual([
+        tExpect(result).toStrictEqual([
             { inlayHintType: 'parameter', position: 99, value: 'value=' },
             { inlayHintType: 'parameter', position: 175, value: 'value=' },
             { inlayHintType: 'parameter', position: 178, value: 'bar=' },
@@ -69,36 +85,41 @@ if (process.platform !== 'win32' || !process.env['CI']) {
             start: { line: 0, character: 0 },
             end: { line: 5, character: 0 },
         });
-        expect(result).toStrictEqual([
+        tExpect(result).toStrictEqual([
             {
                 inlayHintType: 'variable',
                 position: 102,
                 value: ': str',
+                imports: noImports,
             },
         ]);
     });
     test('generics', () => {
         const result = inlayHintSampleFile('generics.py', undefined, { genericTypes: true });
-        expect(result).toStrictEqual([
+        tExpect(result).toStrictEqual([
             {
                 inlayHintType: 'generic',
                 position: 65,
                 value: '[int]',
+                imports: noImports,
             },
             {
                 inlayHintType: 'generic',
                 position: 136,
                 value: '[str]',
+                imports: noImports,
             },
             {
                 inlayHintType: 'generic',
                 position: 185,
                 value: '[int]',
+                imports: noImports,
             },
             {
                 inlayHintType: 'generic',
                 position: 315,
                 value: '[bool]',
+                imports: noImports,
             },
             {
                 inlayHintType: 'parameter',
@@ -109,11 +130,13 @@ if (process.platform !== 'win32' || !process.env['CI']) {
                 inlayHintType: 'generic',
                 position: 332,
                 value: '[Literal[1, 2, 3], ...]',
+                imports: { importFroms: new Map([['typing', new Set(['Literal'])]]), imports: new Set() },
             },
             {
                 inlayHintType: 'generic',
                 position: 441,
                 value: '[list[int], int, int, str]',
+                imports: noImports,
             },
             {
                 inlayHintType: 'parameter',
@@ -124,6 +147,7 @@ if (process.platform !== 'win32' || !process.env['CI']) {
                 inlayHintType: 'generic',
                 position: 581,
                 value: '[tuple[int, str]]',
+                imports: noImports,
             },
         ]);
     });
