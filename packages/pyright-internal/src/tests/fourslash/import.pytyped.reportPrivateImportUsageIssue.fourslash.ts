4a72720278c1f6e998caed2f69ba06ad53e8e0e8
detachhead <detachhead@users.noreply.github.com>
2025-03-29 18:29:42 +1000

    fix `reportPrivateImportUsage` behaving differently when run on an individual file if imports in other files effect the cached import results

---
+++ packages/pyright-internal/src/tests/fourslash/import.pytyped.reportPrivateImportUsageIssue.fourslash.ts
@@ -0,0 +1,42 @@
+/// <reference path="typings/fourslash.d.ts" />
+
+// @filename: pyrightconfig.json
+//// {
+////   "reportPrivateImportUsage": "error"
+//// }
+
+// @filename: a/py.typed
+// @library: true
+////
+
+// @filename: a/__init__.py
+// @library: true
+//// from ._b import C as C
+
+// @filename: a/_b.py
+// @library: true
+//// class C: ...
+//// class _D: ...
+
+// repro of https://github.com/DetachHead/basedpyright/issues/1171
+// the order of these two files matters! b/d.py must be evaluated first to reproduce it
+
+// @filename: b/d.py
+//// from a import [|/*marker2*/C|]
+
+// @filename: b/c/main.py
+//// from a._b import [|/*marker1*/_D|]
+
+helper.openFiles(helper.getMarkers().map((m) => m.fileName));
+// https://github.com/DetachHead/basedpyright/issues/86
+// @ts-expect-error
+await helper.verifyDiagnostics({
+    marker1: {
+        category: 'error',
+        message: `"_D" is not exported from module "a._b"`,
+    },
+    marker2: {
+        category: 'hint',
+        message: `Import "C" is not accessed`,
+    },
+});
