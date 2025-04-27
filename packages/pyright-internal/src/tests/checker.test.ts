6e67a1bf870e4bd85eec157a6306c7692a4269ff
Sahan Serasinghe <sahan.serasinghe@gmail.com>
2025-04-27 12:00:39 +0930

    add unit test

--- packages/pyright-internal/src/tests/checker.test.ts
+++ packages/pyright-internal/src/tests/checker.test.ts
@@ -845,3 +845,11 @@ test('reportInvalidAbstractMethod', () => {
         ],
     });
 });
+
+test('Skip reportAny for explicit object annotation', () => {
+    const configOptions = new ConfigOptions(Uri.empty());
+
+    configOptions.diagnosticRuleSet.reportAny = 'error';
+    const analysisResults = TestUtils.typeAnalyzeSampleFiles(['explicitObjectAnnotation.py'], configOptions);
+    TestUtils.validateResultsButBased(analysisResults, {});
+});
