b9eec164509dd0fed29c3f1faa329480821cc0be
Adrian Ehrsam <adrian.ehrsam@bmsuisse.ch>
2025-05-13 17:16:34 +0200

    prettier

--- packages/pyright-internal/src/analyzer/sourceFile.ts
+++ packages/pyright-internal/src/analyzer/sourceFile.ts
@@ -571,7 +571,12 @@ export class SourceFile {
             //TODO: this isnt ideal because it re-reads the file for each cell which is unnecessary
             source = getIPythonCells(this.fileSystem, this.getRealUri(), this._console)?.[cellIndex]?.source;
             // Make sure we don't return any non-python cells
-            if(source && ["%sql", "%%sql", "%sh", "%pip", "%run", "%fs", "%load", "%matplotlib", "%who", "%env"].includes(source[0].split(/[\s]/)[0])){
+            if (
+                source &&
+                ['%sql', '%%sql', '%sh', '%pip', '%run', '%fs', '%load', '%matplotlib', '%who', '%env'].includes(
+                    source[0].split(/[\s]/)[0]
+                )
+            ) {
                 return undefined;
             }
         } catch (e) {
