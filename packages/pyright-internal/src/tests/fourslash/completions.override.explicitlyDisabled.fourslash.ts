6491a465f1b092baadbb3d7ed3b05f2b4c40c445
STerliakov <terlya.stas@gmail.com>
2025-05-20 17:20:51 +0200

    Remove new static&class method tests, they should also have a decorator and aren't distinct enough to warrant a convoluted test

--- packages/pyright-internal/src/tests/fourslash/completions.override.explicitlyDisabled.fourslash.ts
+++ packages/pyright-internal/src/tests/fourslash/completions.override.explicitlyDisabled.fourslash.ts
@@ -14,14 +14,6 @@
 ////     def method_2(self) -> int:
 ////         pass
 ////
-////     @classmethod
-////     def method_3(self, b) -> str:
-////         pass
-////
-////     @staticmethod
-////     def method_4(self, b) -> str:
-////         pass
-////
 //// class C(B):
 ////     def [|method/*marker*/|]
 
@@ -39,26 +31,10 @@ await helper.verifyCompletion('included', 'markdown', {
             },
             {
                 label: 'method_2',
-                kind: Consts.CompletionItemKind.Method,
+                kind: Consts.CompletionItemKind.Property,
                 textEdit: {
                     range: helper.getPositionRange('marker'),
                     newText: 'method_2(self) -> int:\n    return super().method_2',
-                },
-            },
-            {
-                label: 'method_3',
-                kind: Consts.CompletionItemKind.Method,
-                textEdit: {
-                    range: helper.getPositionRange('marker'),
-                    newText: 'method_3(self, b) -> str:\n    return super().method_3(b)',
-                },
-            },
-            {
-                label: 'method_4',
-                kind: Consts.CompletionItemKind.Method,
-                textEdit: {
-                    range: helper.getPositionRange('marker'),
-                    newText: 'method_4(self, b) -> str:\n    return super().method_4(b)',
                 },
             },
         ],
