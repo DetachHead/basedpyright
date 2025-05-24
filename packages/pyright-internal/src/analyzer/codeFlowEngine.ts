409a891443690d67da9b3b4a9a64fd33b63299a5
detachhead <detachhead@users.noreply.github.com>
2025-05-24 13:04:47 +1000

    prevent narrowed types from being added to unions after the narrowing if statement as they can lead to unnecessary `reportUnknown...` errors

--- packages/pyright-internal/src/analyzer/codeFlowEngine.ts
+++ packages/pyright-internal/src/analyzer/codeFlowEngine.ts
@@ -666,7 +666,7 @@ export function getCodeFlowEngine(
                             }
                         }
 
-                        return getTypeFromBranchFlowNode(curFlowNode as FlowLabel);
+                        return getTypeFromBranchFlowNode(branchFlowNode);
                     }
 
                     if (curFlowNode.flags & FlowFlags.LoopLabel) {
@@ -934,31 +934,34 @@ export function getCodeFlowEngine(
                 }
             }
 
-            function getTypeFromBranchFlowNode(branchNode: FlowLabel): FlowNodeTypeResult {
+            function getTypeFromBranchFlowNode(branchNode: FlowBranchLabel): FlowNodeTypeResult {
                 const typesToCombine: Type[] = [];
 
-                let sawIncomplete = false;
+                const preBranchAntecedentType = branchNode.preBranchAntecedent
+                    ? getTypeFromFlowNode(branchNode.preBranchAntecedent).type
+                    : undefined;
 
+                let sawIncomplete = false;
                 for (const antecedent of branchNode.antecedents) {
                     const flowTypeResult = getTypeFromFlowNode(antecedent);
-
                     if (reference === undefined && flowTypeResult.type && !isNever(flowTypeResult.type)) {
                         // If we're solving for "reachability", and we have now proven
                         // reachability, there's no reason to do more work. The type we
                         // return here doesn't matter as long as it's not undefined.
                         return setCacheEntry(branchNode, UnknownType.create(), /* isIncomplete */ false);
                     }
-
                     if (flowTypeResult.isIncomplete) {
                         sawIncomplete = true;
                     }
-
-                    if (flowTypeResult.type) {
+                    if (!preBranchAntecedentType && flowTypeResult.type) {
                         typesToCombine.push(flowTypeResult.type);
                     }
                 }
 
-                const effectiveType = typesToCombine.length > 0 ? combineTypes(typesToCombine) : undefined;
+                const effectiveType =
+                    // if there's a preBranchAntecedent, we discard the types from the antecendents after the if statement
+                    // and use its type instead to prevent it from creating a union of redundant types
+                    preBranchAntecedentType ?? (typesToCombine.length > 0 ? combineTypes(typesToCombine) : undefined);
 
                 return setCacheEntry(branchNode, effectiveType, sawIncomplete);
             }
