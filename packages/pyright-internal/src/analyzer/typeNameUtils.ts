a22037926381304ea42749980b67314643d2f6f7
Kurt Böhm <kurbo96@gmail.com>
2025-11-13 08:57:51 +0100

    Add useful completions when assigning to an instance of an `Enum` class - If the `Enum` class (or, if it is a nested type, the outermost enclosing class) is locally accessible (e.g. through an import), add each combination of a local name and an `Enum` value to the completions. - If the `Enum` class is not locally accessible yet, add completions for each `Enum` value with auto-import information, including support for enclosed types. - To facilitate these completions, add function to find all local names of a type and one to determine the names of a class and its enclosing classes (if present). - Add some completion tests.

---
+++ packages/pyright-internal/src/analyzer/typeNameUtils.ts
@@ -0,0 +1,77 @@
+import { getEnclosingClass } from './parseTreeUtils';
+import { Scope } from './scope';
+import { SymbolTable } from './symbol';
+import { TypeEvaluator } from './typeEvaluatorTypes';
+import { ClassType, isClass, isModule, ModuleType, TypeBase } from './types';
+
+function handleSymbolTable(evaluator: TypeEvaluator, type: ClassType | ModuleType, symbolTable: SymbolTable): string[] {
+    const getFullName = (t: ClassType | ModuleType) => (isClass(t) ? t.shared.fullName : t.priv.moduleName);
+    const fullName = getFullName(type);
+
+    // The symbols in symbolTable whose type is a non-instance class or a module
+    const symbols: { type: ClassType | ModuleType; localName: string; fullName: string }[] = [];
+    symbolTable.forEach((symbol, symbolName) => {
+        const symbolType = evaluator.getEffectiveTypeOfSymbol(symbol);
+        if ((isClass(symbolType) && !TypeBase.isInstance(symbolType)) || isModule(symbolType)) {
+            symbols.push({ type: symbolType, localName: symbolName, fullName: getFullName(symbolType) });
+        }
+    });
+
+    // If there are symbols whose type has the same full name as `type`, return these
+    const exactMatches = symbols.filter((symbolInfo) => symbolInfo.fullName === fullName);
+    if (exactMatches.length > 0) return exactMatches.map((symbolInfo) => symbolInfo.localName);
+
+    // If there are no full matches, try to find types whose name is a prefix to `type`’s full name
+    // and then recursively try to find a symbol with `type`’s full type.
+    return symbols
+        .filter((symbolInfo) => fullName.startsWith(symbolInfo.fullName))
+        .flatMap((symbolInfo) =>
+            getLocalTypeNameRec(evaluator, type, symbolInfo.type).map((name) => `${symbolInfo.localName}.${name}`)
+        );
+}
+
+function getLocalTypeNameRec(
+    evaluator: TypeEvaluator,
+    type: ClassType | ModuleType,
+    symbolType: ClassType | ModuleType
+) {
+    return handleSymbolTable(
+        evaluator,
+        type,
+        isClass(symbolType) ? ClassType.getSymbolTable(symbolType) : symbolType.priv.fields
+    );
+}
+
+/**
+ * Determine the names (potentially including `.`) which can be used to reference `type` within `scope`.
+ *
+ * This function goes through the symbols defined within `scope` and its parent to find classes/modules
+ * whose full name is a prefix of `type`’s full name. For each of these, it recursively searches the symbols
+ * defined in that class/module for other classes/modules whose full name is a prefix of `type`’s full name
+ * until a type with `type`’s full name is found.
+ */
+export function getLocalTypeNames(evaluator: TypeEvaluator, type: ClassType | ModuleType, scope: Scope): string[] {
+    const out: string[] = [];
+    let currentScope: Scope | undefined = scope;
+    while (currentScope) {
+        out.push(...handleSymbolTable(evaluator, type, currentScope.symbolTable));
+        currentScope = currentScope.parent;
+    }
+    return out;
+}
+
+/**
+ * Determine the parts names of `type` and its enclosing classes from least to most nested class.
+ *
+ * For example, if class `C` is defined in class `B`, which in turn is defined in class `A`,
+ * this function returns `['A', 'B', 'C']`.
+ */
+export function getNestedClassNameParts(type: ClassType): string[] {
+    const parts: string[] = [type.shared.name];
+    let enclosingNode = type.shared.declaration ? getEnclosingClass(type.shared.declaration?.node, false) : undefined;
+    while (enclosingNode) {
+        parts.push(enclosingNode.d.name.d.value);
+        enclosingNode = getEnclosingClass(enclosingNode, false);
+    }
+    return parts.reverse();
+}
