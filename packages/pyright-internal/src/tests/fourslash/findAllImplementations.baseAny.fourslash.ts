e099e59b30a1f5d92484002f7b585cd92bf80af1
beauxq <beauxq@yahoo.com>
2025-11-17 21:21:25 -0800

    clean test file

--- packages/pyright-internal/src/tests/fourslash/findAllImplementations.baseAny.fourslash.ts
+++ packages/pyright-internal/src/tests/fourslash/findAllImplementations.baseAny.fourslash.ts
@@ -2,27 +2,26 @@
 
 // @filename: class_test.py
 //// from typing import Any
-//// 
-//// 
+////
+////
 //// class [|/*marker*/Foo|]:
 ////     ...
-//// 
-//// 
+////
+////
 //// class Baz(Any):  # Any alone doesn't qualify as implementation
 ////     ...
-//// 
-//// 
+////
+////
 //// class [|Bar|](Foo, Any):
 ////     ...
-//// 
-//// 
+////
+////
 //// class Qux(Baz):
 ////     ...
-//// 
-//// 
+////
+////
 //// class [|Spam|](Bar):
 ////     ...
-
 
 {
     const ranges = helper.getRanges();
