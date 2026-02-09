1091050a7fb9485f69ae1e8901dd038c02901472
worldmozara <worldmozara@163.com>
2026-02-09 22:33:24 +0800

    add tests for typed cls default

--- packages/pyright-internal/src/tests/fourslash/removeSelfClsDefault.invokeCodeAction.fourslash.ts
+++ packages/pyright-internal/src/tests/fourslash/removeSelfClsDefault.invokeCodeAction.fourslash.ts
@@ -7,11 +7,16 @@
 
 // @filename: test.py
 //// class Foo:
-////     def bar(self[|/*default*/ = None|]):
+////     def bar(self[|/*default*/=None|]):
+////         pass
+////
+//// class Bar:
+////     def baz(cls: "type[Bar]"[|/*typed_default*/ = None|]):
 ////         pass
 
 {
     const defaultRange = helper.getPositionRange('default');
+    const typedDefaultRange = helper.getPositionRange('typed_default');
     const codeActionTitle = 'Remove default value from parameter';
 
     const codeActions = {
@@ -21,6 +26,15 @@
                 edit: {
                     changes: {
                         'file:///test.py': [{ range: defaultRange, newText: '' }],
+                    },
+                },
+                kind: 'quickfix',
+            },
+            {
+                title: codeActionTitle,
+                edit: {
+                    changes: {
+                        'file:///test.py': [{ range: typedDefaultRange, newText: '' }],
                     },
                 },
                 kind: 'quickfix',
