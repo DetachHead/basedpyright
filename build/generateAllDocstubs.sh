55769d6a28b348ff0aaa3cc5e0917f0823f70b3c
detachhead <detachhead@users.noreply.github.com>
2026-02-12 23:37:22 +1000

    fix crash generating docstubs for older python versions caused by breaking change in uv

--- build/generateAllDocstubs.sh
+++ build/generateAllDocstubs.sh
@@ -1,6 +1,6 @@
 set -e
 for pythonVersion in 3.14 3.13 3.12 3.11 3.10 3.9; do
-    ./pw uv venv --python $pythonVersion
+    ./pw uv venv --clear --python $pythonVersion
     ./pw uv sync --only-group=docstubs --no-install-project
     ./pw uv run --no-sync build/py3_8/generate_docstubs.py
 done
