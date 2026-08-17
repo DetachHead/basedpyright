426e1daa117082564c9e7f0de135ea0734a1c928
Tommy Han <tommyhonym@gmail.com>
2026-08-17 18:11:39 +0800

    update test to fix with the current changes, add back "text" identification for code block.

--- packages/pyright-internal/src/tests/hoverProvider.test.ts
+++ packages/pyright-internal/src/tests/hoverProvider.test.ts
@@ -602,7 +602,7 @@ test('TypedDict doc string', async () => {
     state.openFile(marker1.fileName);
 
     state.verifyHover('markdown', {
-        marker: '```python\n(class) TypedDict\n```\n---\nA simple typed namespace. At runtime it is equivalent to a plain dict.',
+        marker: '```python\n(class) TypedDict\n```\n---\n```\nA simple typed namespace. At runtime it is equivalent to a plain dict.\n```',
     });
 });
 
